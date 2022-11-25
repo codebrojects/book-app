a1d0214205950da4cebe625fb12b8d32f0c8673b
Benos <benhanover4@gmail.com>
2022-11-25 12:10:13 +0200

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [books, setBooks] = useState([]);
+  const [shouldShowBook, setShouldShowBook] = useState(false);
+  const [bookToDisplay, setBookToDisplay] = useState([]);
+  const [wishList, setWishList] = useState([]);
+
+  const onInputChange = (e) => {
+    setValue(e.target.value);
+  };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${value}`
+      );
+      return response.data.items;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const getAllBooks = async () => {
+    if (value.length >= 3 && value.length <= 10) {
+      const resBooks = await fetchData();
+      setBooks(resBooks);
+    }
+  };
+
+  const showBook = (e) => {
+    setShouldShowBook(true);
+    const bookName = e.target.innerText;
+    const bookObj = books.find((book) => {
+      if (book.volumeInfo.title === bookName) return book;
+    });
+    setBookToDisplay(bookObj.volumeInfo);
+  };
+
+  const addBookToWishList = () => {
+    setWishList([...wishList, bookToDisplay]);
+  };
+
+  useEffect(() => {
+    getAllBooks();
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input value={value} onChange={onInputChange} />
+      <div className='container'>
+        <ul>
+          {books?.map((book, index) => (
+            <li key={index}>
+              <span onClick={showBook}>{book.volumeInfo.title}</span>
+            </li>
+          ))}
+        </ul>
+        {shouldShowBook && (
+          <div>
+            <p>author: {bookToDisplay.authors}</p>
+            <button onClick={addBookToWishList}>add to wishList</button>
+          </div>
+        )}
+        <div>
+          {wishList?.map((book, index) => {
+            return (
+              <li key={index}>
+                <span>{book.title}</span>
+              </li>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
